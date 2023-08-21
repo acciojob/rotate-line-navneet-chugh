cb6f9cc2a0af0456a2a64a764eb3fc995a0e5e48
navneet-chugh <navneetchugh15@gmail.com>
2023-08-21 21:42:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function rotateLine() {
+  const line = document.getElementById('line');
+  let rotation = 0;
+
+  function animateRotation() {
+    rotation += 1;
+    line.style.transform = `translate(-50%, -50%) rotate(${rotation}deg)`;
+    requestAnimationFrame(animateRotation);
+  }
+
+  animateRotation();
+}
+rotateLine();
